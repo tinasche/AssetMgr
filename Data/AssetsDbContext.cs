c89c8dadad3daeac7a44e71030a2cd302c02c762
Tinashe Chitakunye <36472653+tinasche@users.noreply.github.com>
2022-06-20 16:30:55 +0200

    refactor(context): change line numbers in the class

--- Data/AssetsDbContext.cs
+++ Data/AssetsDbContext.cs
@@ -10,46 +10,13 @@ namespace AssetManager.Data
         public DbSet<Asset> Assets { get; set; }
         public DbSet<User> Users { get; set; }
 
-        private List<Asset> theAssets = new List<Asset> {new Asset
-                {
-                    Id = 1,
-                    Name = "Samsung Galaxy M11",
-                    Type = "smartphone",
-                    SerialNumber = "0094KC029M",
-                    Manufacturer = "Samsung",
-                    AcquiredOn = System.DateTime.Now,
-                    CurrentUser = 1
-                },
-                new Asset
-                {
-                    Id = 2,
-                    Name = "Samsung Galaxy Tab S4-A",
-                    Type = "tablet",
-                    SerialNumber = "099393V54VT",
-                    Manufacturer = "Samsung",
-                    AcquiredOn = System.DateTime.Now,
-                    CurrentUser = 2
-                },
-                new Asset
-                {
-                    Id = 3,
-                    Name = "Lenovo Thinkpad T480s",
-                    Type = "laptop",
-                    SerialNumber = "PC3029m4",
-                    Manufacturer = "Lenovo",
-                    AcquiredOn = System.DateTime.Now,
-                    CurrentUser = 1
-                },
-                new Asset
-                {
-                    Id = 4,
-                    Name = "Mac Book Pro Express",
-                    Type = "laptop",
-                    SerialNumber = "MC343302",
-                    Manufacturer = "Apple",
-                    AcquiredOn = System.DateTime.Now,
-                    CurrentUser = 3
-                }};
+        private List<Asset> theAssets = new List<Asset> 
+        { 
+            new Asset { Id = 1, Name = "Samsung Galaxy M11", Type = "smartphone", SerialNumber = "0094KC029M", Manufacturer = "Samsung", AcquiredOn = System.DateTime.Now, CurrentUser = 1 },
+            new Asset { Id = 2, Name = "Samsung Galaxy Tab S4-A", Type = "tablet", SerialNumber = "099393V54VT", Manufacturer = "Samsung", AcquiredOn = System.DateTime.Now, CurrentUser = 2},
+            new Asset { Id = 3, Name = "Lenovo Thinkpad T480s", Type = "laptop", SerialNumber = "PC3029m4", Manufacturer = "Lenovo", AcquiredOn = System.DateTime.Now, CurrentUser = 1 },
+            new Asset { Id = 4, Name = "Mac Book Pro Express", Type = "laptop", SerialNumber = "MC343302", Manufacturer = "Apple", AcquiredOn = System.DateTime.Now, CurrentUser = 3 }
+         };
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -65,10 +32,6 @@ namespace AssetManager.Data
                 new User { Id = 2, Username = "Kimberly Manyonga", Department = "Research and Development", UserId = Guid.NewGuid(), JoinedOn = DateTime.Now } ,
                 new User { Id = 3, Username = "Richard Dzingai", Department = "Digital Marketing", UserId = Guid.NewGuid(), JoinedOn = DateTime.Now }
             );
-
-
-
-
         }
     }
 }
